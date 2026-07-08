a8e9253fe842a6e0099f1e7ac4d0f43465654cb1
moazizdev <net_66@yahoo.com>
2026-07-08 14:51:26 +0300

    fix: filter unsupported requests in SW fetch handler

--- sw.js
+++ sw.js
@@ -22,6 +22,7 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || !event.request.url.startsWith("http")) return;
   event.respondWith(
     caches.match(event.request).then((cached) => {
       const fetchPromise = fetch(event.request).then((response) => {
